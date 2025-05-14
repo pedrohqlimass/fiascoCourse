2039beca3acb88225237c7c6744e7d6fb3c14c17
Pedro Henrique Lima <pedroconicz@gmail.com>
2025-05-14 19:33:12 -0300

    Adiciona LacosDeRepeticao.java

---
+++ src/Condicoes/LacosDeRepeticao.java
@@ -0,0 +1,27 @@
+package Condicoes;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+
+        /*
+        *Lacços de Repetição vão  repetir infinitamente ou até atingir o limite
+        * WHILE = FOR
+        * */
+
+        //WHILE
+        // while (condicao) {tudo aqui vai acontecer}
+
+        int numeroDeClones = 0;
+        int numeroMaximoDeClones = 40;
+
+        /*while (numeroDeClones <= numeroMaximoDeClones) {
+            numeroDeClones++;
+            System.out.println("O Naruto fez um clones");*/
+            
+            //FOR
+            for (int i = 0; i <= numeroMaximoDeClones; i++) {
+                System.out.println("O naruto esta se clonando e ja se clonou: " + i);
+
+        }
+    }
+}
