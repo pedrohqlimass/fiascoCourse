38e65cfe7513cadcb122d60ccadf814e81746401
Pedro Henrique Lima <pedroconicz@gmail.com>
2025-05-28 23:39:13 -0300

    Aprendendo sobre Classes e Objetos no Java

---
+++ src/NivelIntermediario/Main.java
@@ -0,0 +1,25 @@
+package NivelIntermediario;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Criar Ninja Naruto
+        Ninja narutoUzumaki = new Ninja();
+        narutoUzumaki.nome = "Naruto Uzumaki";
+        narutoUzumaki.aldeia = "Aldeia da Folha";
+        narutoUzumaki.idade = 17;
+
+        //Criar Ninja Sasuke
+        Ninja sasukeUchiha = new Ninja();
+        sasukeUchiha.nome = "Sasuke Uchiha";
+        sasukeUchiha.aldeia = "Aldeia da Folha";
+        sasukeUchiha.idade = 18;
+
+        //Criar Sakura
+        Ninja sakuraHaruno = new Ninja();
+        sakuraHaruno.nome = "Sakura Haruno";
+        sakuraHaruno.aldeia = "Aldeia da Folha";
+        sakuraHaruno.idade = 19;
+
+    }
+}
