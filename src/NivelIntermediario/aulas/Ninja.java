f9394e5b91ef84550de907fc4752094c4da02f33
Pedro Henrique Lima <pedroconicz@gmail.com>
2025-06-04 19:53:33 -0300

    removendo exemplos de metodos e parâmetro

--- src/NivelIntermediario/aulas/Ninja.java
+++ src/NivelIntermediario/aulas/Ninja.java
@@ -5,25 +5,4 @@ public class Ninja {
     String nome;
     String aldeia;
     int idade;
-    /*
-    * O metodo VOID nao retorna nenhum valor
-    * */
-    //Criar uma metodo Publico personalizado
-    public void SharinganAtivado(){
-        System.out.println("O Sharingan Ativou!");
-    }
-
-    /*
-    * Metodo String vai ter que retornar um STRING
-    * */
-    public String euSouUmNinja(){
-        return "Oi eu sou um Ninja";
-    }
-    /*
-    * Metodo int vai ter que retornan um INT
-    * */
-
-    public int anosParaSeTornarHokage(int idadeMininaParaSerHokage){
-        return idadeMininaParaSerHokage - idade;
-    }
 }
